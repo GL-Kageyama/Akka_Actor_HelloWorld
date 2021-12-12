11dc2c76a19ee3af3665c204c2d063d9487a94c0
GL-Kageyama <36861752+GL-Kageyama@users.noreply.github.com>
2021-12-12 18:38:33 +0900

    Update Main.scala

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -30,11 +30,21 @@ object Main extends App {
 // ===================================
 //           Output Sample
 // ===================================
-// $ cd hello-world/
+// $ cd hello-world 
 // $ sbt
-// ...
-// $ ~run
-// ...
+// [info] welcome to sbt 1.5.5 (Homebrew Java 11.0.12)
+// [info] loading global plugins from /Users/GL-Kageyama/.sbt/1.0/plugins
+// [info] loading project definition from /Users/GL-Kageyama/Desktop/Scala/ActorSample/hello-world/project
+// [info] loading settings for project hello-world from build.sbt ...
+// [info] set current project to hello-akka (in build file:/Users/GL-Kageyama/Desktop/Scala/ActorSample/hello-world/)
+// [info] sbt server started at local:///Users/GL-Kageyama/.sbt/1.0/server/b70794d799a5b96a5f66/sock
+// [info] started sbt server
+// sbt:hello-akka> ~run
+// [info] compiling 1 Scala source to /Users/GL-Kageyama/Desktop/Scala/ActorSample/hello-world/target/scala-2.13/classes ...
+// [info] running Main 
+// SLF4J: Failed to load class "org.slf4j.impl.StaticLoggerBinder".
+// SLF4J: Defaulting to no-operation (NOP) logger implementation
+// SLF4J: See http://www.slf4j.org/codes.html#StaticLoggerBinder for further details.
 // Hello World !
 // Hello Scala !
 // Hello Akka !
